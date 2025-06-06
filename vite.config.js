f3572e976d7b6ca7c8fea88b824623887a5e5453
jaderq <jaderq01@gmail.com>
2025-06-05 23:57:42 -0500

    v1.0.01

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,18 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 import dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.config(); // Carga .env manualmente
 
 export default defineConfig({
-  root: 'src',
-  base: '/control-ingresos/',
+  root: './',
   build: {
     outDir: '../dist',
-    emptyOutDir: true, 
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'src/index.html'),
-        login: resolve(__dirname, 'src/login.html'),
-        dashboard: resolve(__dirname, 'src/dashboard.html'),
-        notFound: resolve(__dirname, 'src/404.html'),
+        index: 'index.html',      
+        login: 'login.html',     
+        dashboard: 'dashboard.html',
+        notfound: '404.html'
       },
     },
   },
